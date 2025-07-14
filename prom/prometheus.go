8c6d277b9ea62588cb2562f45e326375200cf7e5
Harry Metske <harry.metske@gmail.com>
2025-07-14 12:35:38 +0200

    catch npe when no metrics have been gathered yet

--- prom/prometheus.go
+++ prom/prometheus.go
@@ -28,36 +28,38 @@ func StartPrometheusSender() {
 }
 
 func pushMetrics() {
-	conf.HzSliceLock.Lock()
-	// determine the average, and the empty the slice again
-	var total int64
-	var averageGet, averagePut int64
-	for _, value := range conf.HzGetTimes {
-		total = total + value
-	}
-	averageGet = total / int64(len(conf.HzGetTimes))
-	total = 0
-	for _, value := range conf.HzPutTimes {
-		total = total + value
-	}
-	averagePut = total / int64(len(conf.HzPutTimes))
-	conf.HzGetTimes = conf.HzGetTimes[:0]
-	conf.HzPutTimes = conf.HzPutTimes[:0]
-	conf.HzSliceLock.Unlock()
+	if len(conf.HzGetTimes) > 0 && len(conf.HzPutTimes) > 0 {
+		conf.HzSliceLock.Lock()
+		// determine the average, and the empty the slice again
+		var total int64
+		var averageGet, averagePut int64
+		for _, value := range conf.HzGetTimes {
+			total = total + value
+		}
+		averageGet = total / int64(len(conf.HzGetTimes))
+		total = 0
+		for _, value := range conf.HzPutTimes {
+			total = total + value
+		}
+		averagePut = total / int64(len(conf.HzPutTimes))
+		conf.HzGetTimes = conf.HzGetTimes[:0]
+		conf.HzPutTimes = conf.HzPutTimes[:0]
+		conf.HzSliceLock.Unlock()
 
-	metricGet := prometheus.NewGauge(prometheus.GaugeOpts{Name: metricName, Help: metricHelp})
-	metricPut := prometheus.NewGauge(prometheus.GaugeOpts{Name: metricName, Help: metricHelp})
-	metricGet.Set(float64(averageGet))
-	metricPut.Set(float64(averagePut))
+		metricGet := prometheus.NewGauge(prometheus.GaugeOpts{Name: metricName, Help: metricHelp})
+		metricPut := prometheus.NewGauge(prometheus.GaugeOpts{Name: metricName, Help: metricHelp})
+		metricGet.Set(float64(averageGet))
+		metricPut.Set(float64(averagePut))
 
-	if err := push.New(conf.PushGatewayURL, "rabo_hzmon").Grouping("sourceIP", conf.MyIP).Grouping("operation", "get").Collector(metricGet).Push(); err != nil {
-		log.Printf("failed to send get metrics to %s: %v\n", conf.PushGatewayURL, err.Error())
-	} else {
-		util.LogDebug(fmt.Sprintf("get metric (%d) sent to push gateway @ %s", averageGet, conf.PushGatewayURL))
-	}
-	if err := push.New(conf.PushGatewayURL, "rabo_hzmon").Grouping("sourceIP", conf.MyIP).Grouping("operation", "put").Collector(metricPut).Push(); err != nil {
-		log.Printf("failed to send put metrics to %s: %v\n", conf.PushGatewayURL, err.Error())
-	} else {
-		util.LogDebug(fmt.Sprintf("put metric (%d) sent to push gateway @ %s", averagePut, conf.PushGatewayURL))
+		if err := push.New(conf.PushGatewayURL, "rabo_hzmon").Grouping("sourceIP", conf.MyIP).Grouping("operation", "get").Collector(metricGet).Push(); err != nil {
+			log.Printf("failed to send get metrics to %s: %v\n", conf.PushGatewayURL, err.Error())
+		} else {
+			util.LogDebug(fmt.Sprintf("get metric (%d) sent to push gateway @ %s", averageGet, conf.PushGatewayURL))
+		}
+		if err := push.New(conf.PushGatewayURL, "rabo_hzmon").Grouping("sourceIP", conf.MyIP).Grouping("operation", "put").Collector(metricPut).Push(); err != nil {
+			log.Printf("failed to send put metrics to %s: %v\n", conf.PushGatewayURL, err.Error())
+		} else {
+			util.LogDebug(fmt.Sprintf("put metric (%d) sent to push gateway @ %s", averagePut, conf.PushGatewayURL))
+		}
 	}
 }
